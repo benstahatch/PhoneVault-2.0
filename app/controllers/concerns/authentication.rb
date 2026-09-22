9074f50f073639985631ee5d24605da7a44e74bb
Ben Hatcher <benstahatch@users.noreply.github.com>
2026-09-21 23:38:50 -0500

    Add authentication foundation

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,14 @@
+module Authentication
+  extend ActiveSupport::Concern
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+
+  def require_login
+    redirect_to login_path unless current_user
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
